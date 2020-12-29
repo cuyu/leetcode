5767d2daa479f58923e2fa404e24c2085e00f554
cuyu <icyarm@qq.com>
2020-12-29 23:50:26 +0800

    Use O(n) solution

--- 670.最大交换.py
+++ 670.最大交换.py
@@ -9,29 +9,19 @@ class Solution:
     def maximumSwap(self, num: int) -> int:
         n = str(num)
         s = [x for x in n]
-        low = 0
-        high = len(s) - 1
-        replaced = -1
-        while low < len(s):
-            if low >= high:
-                if replaced != -1:
-                    break
-                low += 1
-                high = len(s) - 1
-            else:
-                if s[low] < s[high]:
-                    if replaced == -1:
-                        s[low], s[high] = s[high], s[low]
-                    else:
-                        # 如果找到了更大的数，则先把之前交换的复原，再以新的更大的数做交换
-                        s[low], s[replaced] = s[replaced], s[low]
-                        s[low], s[high] = s[high], s[low]
-                    replaced = high
-                high -= 1
-        return int(''.join(s))
 
+        last = {}
+        for i, c in enumerate(s):
+            last[c] = i
+        for i, c in enumerate(s):
+            for j in range(9, int(c), -1):
+                x = str(j)
+                if x in last and last[x] > i:
+                    s[i], s[last[x]] = s[last[x]], s[i]
+                    return int(''.join(s))
+        return num
 # @lc code=end
 
 
 if __name__ == "__main__":
-    Solution().maximumSwap(120)
+    Solution().maximumSwap(10)
